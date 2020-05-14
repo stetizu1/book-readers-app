c919a63afca24b2cf86cf4c3edcb6cd7e2934e05
Zuzana Štětinová <stetinovaz@gmail.com>
2020-05-14 18:24:46 +0200

    reuse stars

--- packages/frontend/src/app/components/common/blockCreators/getStars.tsx
+++ packages/frontend/src/app/components/common/blockCreators/getStars.tsx
@@ -1,32 +1,6 @@
-import React, { FC, ReactElement } from 'react';
-import { Star, StarBorder } from '@material-ui/icons';
+import React from 'react';
+import { getRatingFormItem } from 'app/components/common/blockCreators/form/rating/getRatingFormItem';
 
-import { starsCount } from 'book-app-shared/constants/Stars';
-
-import { repeat } from 'app/helpers/repeat';
-
-import { useCardItemStyle } from 'app/components/common/styles/cardItems/CardItemStyle';
-import { isNull, isUndefined } from 'book-app-shared/helpers/typeChecks';
-import { composeClasses } from 'app/helpers/style/composeClasses';
-
-
-export type StarsData = {
-  stars: number | null | undefined;
-};
-
-const BaseStars: FC<StarsData> = ({ stars }) => {
-  const classes = useCardItemStyle();
-  if (isUndefined.or(isNull)(stars)) return null;
-  return (
-    <div className={composeClasses(classes.item, classes.itemContainer)}>
-      {repeat(Star, stars)}
-      {repeat(StarBorder, starsCount.max - stars)}
-    </div>
-  );
-};
-
-export type StarsComponentType = ReactElement<StarsData>;
-
-export const getStars = (stars: number | undefined | null): StarsComponentType => (
-  <BaseStars stars={stars} />
+export const getStars = (stars: number | undefined | null): JSX.Element => (
+  getRatingFormItem({ readOnly: true, value: stars, label: null })
 );
